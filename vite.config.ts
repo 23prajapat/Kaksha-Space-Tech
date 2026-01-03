bbca7e80b5709487b0975181d7b036b9381d34cc
Jagrti Prajapat <prajapatjagrti023.com>
2026-01-03 18:47:39 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/my-company-site/'
+  base: '/Kaksha-Space-Tech/'
 })
